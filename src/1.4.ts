f57990b0e108d28cd4594563513730701fd1884f
rakib-sikder <itsmebroiam@gmail.com>
2025-05-04 00:35:14 +0600

    Add TypeScript variables and data structures for personal information

---
+++ src/1.4.ts
@@ -0,0 +1,11 @@
+let Name:string='Rakib Sikder'
+
+const gender:string="male"
+let age:number=23
+let isMarried:boolean= false
+let hobbies:string[]=["cricket","football","badminton"]
+let address :{city: string, country:string}={
+    city:"Dhaka",
+    country:"Bangladesh"
+}
+let person :[string,number,boolean]=["Rakib",23,false]
