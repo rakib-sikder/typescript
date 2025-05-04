0653d64ac3f812299f906bd94d5ff63e10744e53
rakib-sikder <itsmebroiam@gmail.com>
2025-05-04 10:21:23 +0600

    Add user object with TypeScript type definitions and initial data

---
+++ src/1.5.ts
@@ -0,0 +1,17 @@
+// object
+const user:{
+    company: "Google",
+   fastname: string,
+middlename?: string,
+    lastname: string,
+    age: number,
+    isMarried: boolean,
+}={
+    company:"Google",
+    fastname:"Rakib",
+    lastname:"Sikder",
+    age:23,
+    isMarried:false,
+}
+
+
